1c45737ae9a5c9bcd6e39ae3eb6caf8dcd2e8694
Pavel Lysenko <rezachez@gmail.com>
2012-09-23 20:44:41 +0300

    Uploader images

--- js/interactive.js
+++ js/interactive.js
@@ -2,5 +2,8 @@ jQuery(function(){
     $('.comments').tooltip({
         placement: 'bottom'
     });
-    $('#redactor_content').redactor();
+    $('#redactor_content').redactor({
+        imageUpload: './upload.php',
+        imageGetJson: false
+    });
 });
--- upload.php
+++ upload.php
@@ -1,28 +1,7 @@
 <?php
-
-// files storage folder
-$dir = '/files/images/';
-
-$_FILES['file']['type'] = strtolower($_FILES['file']['type']);
-
-if ($_FILES['file']['type'] == 'image/png'
-    || $_FILES['file']['type'] == 'image/jpg'
-    || $_FILES['file']['type'] == 'image/gif'
-    || $_FILES['file']['type'] == 'image/jpeg'
-    || $_FILES['file']['type'] == 'image/pjpeg')
-{
-    // setting file's mysterious name
-    $file = $dir.md5(date('YmdHis')).'.jpg';
-
-    // copying
-    copy($_FILES['file']['tmp_name'], $file);
-
-    // displaying file
-    $array = array(
-        'filelink' => '/images/'.$file
-    );
-
+    $imageInfo = pathinfo($_FILES['file']['name']);
+    $imagePath = "./files/images/notes/" . date('YmdHis') . '.' . $imageInfo['extension'];
+    move_uploaded_file($_FILES['file']['tmp_name'], $imagePath);
+    $array = array('filelink' => $imagePath);
     echo stripslashes(json_encode($array));
-}
-
 ?>
